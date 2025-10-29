f5b5b99a222efdae1bc7ea8362ad42f1a7b3b57c
ICEQ <farisemad12@outlook.com>
2025-10-29 09:55:46 +0300

    Refactor Calculator class to improve operation handling and enhance PrintResult output

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -11,82 +11,83 @@ using namespace std;
  *  - PrintResult:   print the current result to stdout
  */
 
-
 class Calculator {
     private:
-        double number = 0;
-        int X = 0;
-        string LastOperation;
-        int setX(int Number) {
-            return X = Number;
-        }
-        int setClear() {
-            
-            return (number = 0, X = 0) ;
-        }
-        int setAdd(int X) {
-            return number += X;
-        }
-        int setSubtract(int X) {
-            return number -= X;
-        }
-        int setMultiply(int X) {
-            return number *= X;
-        }
-        double setDivide(int X) {
-            if(X ==0)
-                X=1;
-            return number /= X;
+        float _Result = 0;
+        float _LastNumber = 0;
+        string _LastOperation = "Clear";
+        float _PerviousResult = 0;
+
+        bool _IsZero(float Number) {
+            return (Number == 0) ;
         }
     public:
+        void Add(float Number) {
+            _LastNumber = Number;
+            _PerviousResult = _Result;
+            _LastOperation = "Adding";
+            _Result += Number;
+        }
+        void Subtract(float Number) {
+            _LastNumber = Number;
+            _PerviousResult = _Result;
+            _LastOperation = "Subtracting";
+            _Result -= Number;
+        }
+        void Divide(float Number) {
+            _LastNumber = Number;
+            if(_IsZero(Number)) {
+                Number = 1;
+            }
+            _PerviousResult = _Result;
+            _LastOperation = "Divideing";
+            _Result /= Number;
+        }
+        void Multiply(float Number) {
+            _LastNumber = Number;
+            _PerviousResult = _Result;
+            _LastOperation = "Multiplying";
+            _Result *= Number;
+        }
+        float GetFinalResults() {
+            return _Result;
+        }
         void Clear() {
-            setClear();
-            LastOperation = "Clear ";
+            _LastNumber = 0;
+            _PerviousResult = 0;
+            _LastOperation = "Clear";
+            _Result = 0;
         }
-        void Add(int Number) {
-            setX(Number);
-            setAdd(Number);
-            LastOperation = "Add ";
+
+        void CancelLastOperation(){
+            _LastNumber = 0;
+            _LastOperation = "Cancel Last Operation";
+            _Result = _PerviousResult;
         }
-        void Subtract(int Number) {
-            setX(Number);
-            setSubtract(Number);
-            LastOperation = "Subtract ";
-        }
-        void Multiply(int Number) {
-            setX(Number);
-            setMultiply(Number);
-            LastOperation = "Multiply ";
-        }
-        void Divide(int Number) {
-            setX(Number);
-            setDivide(Number);
-            LastOperation = "Divide ";
-        }
+
         void PrintResult() {
-            cout << "\nResult after " << LastOperation << X << " is " << number << endl;
-        }
+            cout << "Result " ;
+            cout << "After " << _LastOperation << " " << _LastNumber << " is: " << _Result << endl;
+        } 
+
+        
 };
 
 int main() {
     Calculator C;
-    C.Add(10);
-    C.PrintResult();
-    C.Add(10);
-    C.PrintResult();
-    C.Add(10);
-    C.PrintResult();
-    C.Add(10);
-    C.PrintResult();
-    C.Add(10);
-    C.PrintResult();
-    C.Subtract(5);
+    C.Add(5);
     C.PrintResult();
     C.Multiply(3);
     C.PrintResult();
-    C.Divide(2);
+    C.Subtract(2);
+    C.PrintResult();
+    C.Divide(4);
     C.PrintResult();
     C.Clear();
+    C.PrintResult();
+    C.Add(10);
+    C.PrintResult();
+    C.CancelLastOperation();
     C.PrintResult();
     return 0;
 }
