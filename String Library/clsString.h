d3a9ac415ec58d5ca83f92dc4170bcad7ae8611c
ICEQ <farisemad12@outlook.com>
2025-11-11 18:59:20 +0200

    Enhance clsString class with additional string manipulation methods and update main.cpp for testing

--- String Library/clsString.h
+++ String Library/clsString.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <limits>
+#include <vector>
 using namespace std;
 
 class clsString {
@@ -126,20 +130,149 @@ class clsString {
         }
         int CountLowerLetters() {
             return CountLowerLetters(_Value);
-        }
-        static int CountLetter(string S1, char C1) {
+        }        
+        static int CountLetter(string S1, char C1, bool MatchCase = true) {
             int count = 0;
             for (int i = 0; i < S1.length(); i++) {
-                if (isalpha(S1[i]) && (C1 == S1[i])) {
-                    count++;
+                if (MatchCase) {
+                    if (isalpha(S1[i]) && ((C1 == S1[i]))) {
+                        count++;
+                    }
+                }
+                else {
+                    if (isalpha(S1[i]) && ((tolower(C1) == tolower(S1[i])))) {
+                        count++;
+                    }
                 }
             }
             return count;
         }
-        int CountLetter() {
-            return CountLetter(_Value, _Letter);
+        int CountLetter(bool MatchCase = true) {
+            return CountLetter(_Value, _Letter, MatchCase);
         }
-        
+        int CountLetter(char Letter, bool MatchCase = true) {
+            return CountLetter(_Value, Letter, MatchCase);
+        }
+        static bool IsVowel(char C1) {
+            C1 = tolower(C1);
+            return (C1 == 'a' || C1 == 'e' || C1 == 'i' || C1 == 'o' || C1 == 'u');
+        }
+        bool IsVowel() {
+            return IsVowel(_Letter);
+        }
+        static int CountVowel(string s1) {
+            int vowel = 0;
+            for (int i = 0; i < s1.length(); i++) {
+                    if (IsVowel(s1[i])) {
+                        vowel++;
+                    }
+            }
+            return vowel;
+        }
+        int CountVowel() {
+            return CountVowel(_Value);
+        }
+        static void PrintEachWord(string S1) {
+            string CurrentWord = "";
+            for (int i = 0; i < S1.length(); i++) {
+                    if (S1[i] != ' ') {
+                        CurrentWord += S1[i];
+                    }
+                    else {
+                        cout << endl;
+                        CurrentWord = "";
+                    }
+
+            }
+            if (CurrentWord != "") {
+                    cout << CurrentWord;
+                    cout << endl;
+            }
+        }
+        void PrintEachWord() {
+            return PrintEachWord(_Value);
+        }
+        static string TrimLeft(string S1) {
+            for (int i = 0; i <= S1.length() - 1; i++) {
+                if (S1[i] != ' ') {
+                    return S1 = S1.substr(i, S1.length() - 1);
+                }
+            }
+            return "";
+        }
+        string TrimLeft() {
+            return TrimLeft(_Value);
+        }
+        static string TrimRight(string S1) {
+            for (int i = S1.length() - 1  ; i >= 0 ; i--) {
+                if (S1[i] != ' ') {
+                    return S1 = S1.substr(0, i + 1);
+                }
+            }
+            return "";
+        }
+        string TrimRight() {
+            return TrimRight(_Value);
+        }
+        static string Trim(string S1) {
+            return TrimLeft(TrimRight(S1));
+        }
+        string Trim() {
+            return Trim(_Value);
+        }
+        static vector<string> SplitString(string S1, string delim) {
+            vector <string> vString;
+            short pos = 0;
+            string sWord;
+            while((pos = S1.find(delim)) != std::string::npos) {
+                sWord = S1.substr(0, pos);
+                if(sWord != "") {
+                    vString.push_back(sWord);
+                }
+                S1.erase(0,pos + delim.length());
+            }
+            if (S1 != "") {
+                vString.push_back(S1);
+            }
+            return vString;
+        }
+        vector<string> SplitString(string delim) {
+            return SplitString( _Value ,delim);
+        }
+
+        static string ReverseString(string S1){
+            vector<string> vString;
+            string S2 = "";
+            vString = SplitString(S1, " ");
+            vector<string>::iterator iter =vString.end();
+            while (iter != vString.begin()) {
+                --iter;
+                S2 += *iter + " ";
+            }
+            S2 = S2.substr(0, S2.length() - 1); // remove last space
+            return S2;
+        }
+        string ReverseString() {
+            return ReverseString(_Value);
+        }
+        static string JoinString(vector<string> vString, string delim) {
+            string S1 = "";
+            for (string& s : vString) {
+                S1 = S1 + s + delim;
+            }
+
+            return S1.substr(0,S1.length() - delim.length());
+        }
+
+        static string JoinString(string arrString[], int len, string delim) {
+            string S1 = "";
+            for (int i = 0; i <= len; i++) {
+                S1 += arrString[i] + delim;
+            }
+
+            return S1.substr(0,S1.length() - delim.length());
+        }
+
 };
 
 
--- String Library/main.cpp
+++ String Library/main.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
-#include "clsString.h";
+#include "clsString.h"
 
 int main() {
     
     clsString MyString1;
-    clsString MyString2("Faris Emad Elden");
+    clsString MyString2("Faris Emad Elden e");
+    MyString2.SetLetter('F');
 
-   
+    cout << MyString2.CountLetter() << endl;
+    cout << MyString2.CountLetter('E', false) << endl;
+    MyString1.SetLetter('a');
+    ;
+    if (MyString1.IsVowel()) {
+        cout << "YES Letter '" << MyString1.GetLetter() << "' is vowel";
+    }
+    else {
+        cout << "NO Letter '" << MyString1.GetLetter()  << "' is not vowel";
+    }
+    MyString1.SetValue("   Faris Emad-eldin   ");
+    cout << endl;
+    cout << MyString1.GetValue();
+    //MyString1.PrintEachWord(); i need to cheack angin is this function is work or no ??
+    cout << endl;
+    // MyString1.PrintEachWord();
+    cout << "trim left  = " << MyString1.TrimLeft() << endl;
+    cout << "trim right = " << MyString1.TrimRight() << endl;
+    cout << "trim       ="  << MyString1.Trim() << endl;
+    cout << "=============================\n";
+    
+    vector<string> vString;
 
-    cout << MyString2.GetValue()<< endl;
-    cout << "count words: " << MyString2.CountEachWord();
-    cout << "count letter:  " << 
+    vString = MyString1.SplitString(" ");
+
+    cout << "\nTokens = " << vString.size() << endl;
+
+    for (string& s : vString)
+    {
+        cout << s << endl;
+    }
+    cout << "=============================\n";
+    //Joins
+    vector<string> vString1 = { "Mohammed","Faid","Ali","Maher" };
+    
+    cout << "\n\nJoin String From Vector: \n";
+    cout << clsString::JoinString(vString1, " ");
+
+
+    string arrString[] = { "Mohammed","Faid","Ali","Maher" };
+
+    cout << "\n\nJoin String From array: \n";
+    cout << clsString::JoinString(arrString, 4, " ");
     return 0;
 }
