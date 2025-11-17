c94af95a682ce596b5dde512f331cbaeccdf1fbf
ICEQ <farisemad12@outlook.com>
2025-11-17 13:26:51 +0200

    Add clsDate and clsInputValidation classes for date management and input validation

---
+++ Input & Validation Library/clsInputValidation.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class clsInputValidation {
+    private:
+
+    public:
+        static bool IsNumberBetween(int number, int from, int to) {
+            
+        }
+
+};
+
