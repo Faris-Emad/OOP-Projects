5b3eb4da6cb8e3c4cdc4a6554b3d15545351bdf5
ICEQ <farisemad12@outlook.com>
2025-12-30 16:48:39 +0300

    Implement user login functionality: add clsLoginScreen for user authentication, modify clsUser to enhance user existence checks, and update main function to show login screen.

---
+++ Bank System/Global.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include "clsUsers.h"
+
+clsUser CurrentUser = clsUser::Find("","");
+
+
+
+
+
---
+++ Bank System/clsLoginScreen.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+#include "clsUsers.h"
+#include "Global.h"
+#include "clsMainScreen.h"
+
+
+using namespace std;
+
+class clsLoginScreen : protected clsScreen
+{
+    private:
+        static void _Login() {
+            bool LoginFaild = false;
+            string UserName = "" , Password = "";
+            do
+            {
+                if(LoginFaild) {
+                    cout << "\nInvlaid User Name / Password!!\n\n";
+                }
+                cout << "Enter User Name: ";
+                UserName = clsInputValidate::ReadString();
+                cout << "\nEnter User Password: ";
+                Password = clsInputValidate::ReadString();
+                CurrentUser = clsUser::Find(UserName, Password);
+                LoginFaild = CurrentUser.IsEmpty();
+            } while (LoginFaild);
+            clsMainScreen::ShowMainMenu();
+        }   
+
+        
+    public:
+        static void ShowLoginScreen() {
+            system("cls");
+            _DrawScreenHeader(" Login Screen ");
+            _Login();
+            
+            
+            
+        }
+
+};
+
+
