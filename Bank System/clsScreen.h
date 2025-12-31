19dc7632dc51ad630c49acccbec216992b4b770d
ICEQ <farisemad12@outlook.com>
2025-12-31 16:23:46 +0300

    Refactor _DrawScreenHeader to include user and date information

--- Bank System/clsScreen.h
+++ Bank System/clsScreen.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "clsUsers.h"
 #include "Global.h"
+#include "clsDate.h"
 
 
 using namespace std;
@@ -10,14 +11,21 @@ class clsScreen
 {
 protected :
     static void _DrawScreenHeader(string Title,string SubTitle ="") {
+        clsDate Date;
         cout << "\t\t\t\t\t______________________________________";
         cout << "\n\n\t\t\t\t\t  " << Title;
         if (SubTitle != "")
         {
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
-        cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\t______________________________________";
+        cout << "\n\t\t\t\t\tUser: " << CurrentUser.GetUserName();
+        cout << "\n\t\t\t\t\tDate: ";
+        Date.Print();
+        cout << endl;
     }
+
+
     static bool CheckAccessRights(clsUser::UserAccess Permission) {
     
         if (!CurrentUser.CheckAccessPermission(Permission)) {
