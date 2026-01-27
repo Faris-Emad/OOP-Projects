0f743e61c797ff6babba54e0acc2f4666b0386e7
ICEQ <farisemad12@outlook.com>
2026-01-27 12:56:32 +0300

    Add clsRegisterLogins class and CMakeLists.txt for project setup

---
+++ Bank System/ clsRegisterLogins.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "clsString.h"
+#include "Global.h"
+using namespace std;
+
+class clsRegisterLogins {
+    public:
+};
